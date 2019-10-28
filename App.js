1f51de2fa9a72eda31ec57a411f080d5b093c05c
Giang Nguyen <thuygiang152@gmail.com>
2019-10-28 21:18:00 +0200

    Adding navigation to app

--- App.js
+++ App.js
@@ -8,165 +8,56 @@ import {
   TextInput,
   FlatList
 } from "react-native";
-import BookCount from "./components/BookCount";
+
+import WelcomeScreen from "./screens/AppSwitchNavigator/WelcomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import CustomDrawerComponent from "./screens/DrawerNavigator/CustomDrawerComponent";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { Ionicons } from "@expo/vector-icons";
-import CustomActionButton from "./components/CustomActionButton";
+import { fromBinary } from "uuid-js";
+const App = () => <AppContainer />;
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      totalCount: 0,
-      readingCount: 0,
-      readCount: 0,
-      isAddNewBookVisible: false,
-      textInput: null,
-      books: []
-    };
+const LoginStackNavigator = createStackNavigator({
+  WelcomeScreen: {
+    screen: WelcomeScreen,
+    navigationOptions: {
+      header: null
+    }
+  },
+  SignUpScreen
+});
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: "Home",
+        drawerIcon: () => <Ionicons name="ios-home" size={24} />
+      }
+    },
+    SettingsScreen: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: "Settings",
+        drawerIcon: () => <Ionicons name="ios-settings" size={24} />
+      }
+    }
+  },
+  {
+    contentComponent: CustomDrawerComponent
   }
+);
 
-  showAddNewBook = () => {
-    this.setState({ isAddNewBookVisible: true });
-  };
+const AppSwitchNavigator = createSwitchNavigator({
+  LoginStackNavigator,
+  AppDrawerNavigator
+});
 
-  hideAddNewBook = () => {
-    this.setState({ isAddNewBookVisible: false });
-  };
-
-  addBook = book => {
-    this.setState({
-      books: [...this.state.books, book],
-      totalCount: this.state.totalCount + 1,
-      readingCount: this.state.readingCount + 1
-    });
-  };
-
-  markAsRead = (selectedBook, index) => {
-    let newList = this.state.books.filter(book => book !== selectedBook);
-    this.setState({
-      books: newList,
-      readCount: this.state.readCount + 1,
-      readingCount: this.state.readingCount - 1
-    });
-  };
-
-  renderItem = (item, index) => (
-    <View
-      style={{
-        height: 50,
-        flexDirection: "row"
-      }}
-    >
-      <View style={{ flex: 1, justifyContent: "center", padding: 5 }}>
-        <Text>{item}</Text>
-      </View>
-      <CustomActionButton onPress={() => this.markAsRead(item, index)}>
-        <Text style={{ fontWeight: "400", color: "#89cff0" }}>
-          Mark as Read
-        </Text>
-      </CustomActionButton>
-    </View>
-  );
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <SafeAreaView>
-          <View
-            style={{
-              height: 70,
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-          >
-            <Text style={{ fontSize: 24, fontWeight: "500" }}>Book Worm</Text>
-          </View>
-          {this.state.isAddNewBookVisible && (
-            <View
-              style={{
-                height: 50,
-                flexDirection: "row",
-                paddingLeft: 8
-              }}
-            >
-              <TextInput
-                clearButtonMode="always"
-                onChangeText={text => this.setState({ textInput: text })}
-                style={{
-                  flex: 1,
-                  marginRight: 3,
-                  paddingLeft: 6,
-                  backgroundColor: "#ececec"
-                }}
-                placeholder="Enter book name"
-              />
-
-              <CustomActionButton
-                onPress={() => this.addBook(this.state.textInput)}
-              >
-                <Ionicons
-                  name="ios-checkmark-circle"
-                  color="#89cff0"
-                  size={50}
-                />
-              </CustomActionButton>
-              <CustomActionButton onPress={this.hideAddNewBook}>
-                <Ionicons name="ios-close-circle" color="#A9A9A9" size={50} />
-              </CustomActionButton>
-            </View>
-          )}
-          <View style={{ height: 530 }}>
-            <FlatList
-              data={this.state.books}
-              renderItem={({ item }, index) => this.renderItem(item, index)}
-              keyExtractor={(item, index) => index.toString()}
-              ListEmptyComponent={
-                <View style={{ marginTop: 50, alignItems: "center" }}>
-                  <Text style={{ fontSize: 20 }}>No books</Text>
-                </View>
-              }
-            />
-            <TouchableOpacity
-              style={{ position: "absolute", bottom: 20, right: 20 }}
-              onPress={this.showAddNewBook}
-            >
-              <View
-                style={{
-                  width: 50,
-                  height: 50,
-                  borderRadius: 25,
-                  backgroundColor: "#89cff0",
-                  alignItems: "center",
-                  justifyContent: "center"
-                }}
-              >
-                <Text
-                  style={{
-                    color: "white",
-                    fontSize: 30
-                  }}
-                >
-                  +
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              height: 70,
-              borderTopWidth: 0.5,
-              borderTopColor: "#E9E9E9",
-              flexDirection: "row"
-            }}
-          >
-            <BookCount title="Title" count={this.state.totalCount} />
-            <BookCount title="Reading" count={this.state.readingCount} />
-            <BookCount title="Read" count={this.state.readCount} />
-          </View>
-        </SafeAreaView>
-      </View>
-    );
-  }
-}
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
 export default App;
--- components/CustomActionButton.js
+++ components/CustomActionButton.js
@@ -1,32 +1,43 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import PropTypes from "prop-types";
+import Proptypes from "prop-types";
+import colors from "../assets/colors";
+function getPosition(position) {
+  switch (position) {
+    case "left":
+      return { position: "absolute", left: 20, bottom: 20 };
+    default:
+      return { position: "absolute", right: 20, bottom: 20 };
+  }
+}
 
-const CustomActionButton = ({ children, onPress }) => {
+const CustomActionButton = ({ children, onPress, style, position }) => {
+  const floatingActionButton = position ? getPosition(position) : [];
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.button}>{children}</View>
+    <TouchableOpacity style={floatingActionButton} onPress={onPress}>
+      <View style={[styles.button, style]}>{children}</View>
     </TouchableOpacity>
   );
 };
 
 CustomActionButton.propTypes = {
-  onPress: PropTypes.func.isRequired,
-  children: PropTypes.element.isRequired,
-  style: PropTypes.object
+  onPress: Proptypes.func.isRequired,
+  children: Proptypes.element.isRequired,
+  style: Proptypes.object
 };
 
 CustomActionButton.defaultProps = {
   style: {}
 };
 
+export default CustomActionButton;
+
 const styles = StyleSheet.create({
   button: {
     width: 50,
+    backgroundColor: colors.bgError,
     height: 50,
-    color: "#89cff0",
     alignItems: "center",
     justifyContent: "center"
   }
 });
-export default CustomActionButton;
