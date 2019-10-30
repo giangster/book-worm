3abeabea57922ed1b4ff7af842003ba271c1f56b
Giang Nguyen <thuygiang152@gmail.com>
2019-10-30 17:34:28 +0200

    Setting up firebase

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkaFDXAR5Z1IW83dYdmj_rScpfMAypMLs",
+  authDomain: "book-worm-14f18.firebaseapp.com",
+  databaseURL: "https://book-worm-14f18.firebaseio.com",
+  projectId: "book-worm-14f18",
+  storageBucket: "book-worm-14f18.appspot.com",
+  messagingSenderId: "794345898666",
+  appId: "1:794345898666:web:9e4c9c38359572228206d3",
+  measurementId: "G-H93PDPM0JJ"
+};
