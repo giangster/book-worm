480ee48f2a80358f68278b042015423028022c7e
Giang Nguyen <thuygiang152@gmail.com>
2019-11-20 22:48:35 +0200

    Activity Indicator for BooksReadingScreen

--- screens/HomeTabNavigator/BooksReadingScreen.js
+++ screens/HomeTabNavigator/BooksReadingScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator
+} from "react-native";
 import colors from "../../assets/colors";
 import ListItem from "../../components/ListItem";
 import { connect } from "react-redux";
@@ -9,6 +15,19 @@ const BooksReadingScreen = props => {
 
   return (
     <View style={styles.container}>
+      {this.props.isLoading && (
+        <View
+          style={{
+            ...StyleSheet.absoluteFill,
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 1000,
+            elevation: 1000
+          }}
+        >
+          <ActivityIndicator size="large" color={colors.logoColor} />
+        </View>
+      )}
       <FlatList
         data={props.books.booksReading}
         renderItem={({ item }, index) => renderItem(item)}
