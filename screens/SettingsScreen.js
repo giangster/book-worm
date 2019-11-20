0c1d2cbb5048a028b5ff9d31e0a02e3f483afdcc
Giang Nguyen <thuygiang152@gmail.com>
2019-11-20 14:51:25 +0200

    Refactoring: refactoring SettingScreen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import CustomActionButton from "../components/CustomActionButton";
@@ -7,35 +7,33 @@ import * as firebase from "firebase";
 import "firebase/auth";
 
 import colors from "../assets/colors";
-class SettingScreen extends Component {
-  signOut = async () => {
+const SettingScreen = props => {
+  const signOut = async () => {
     try {
       await firebase.auth().signOut();
-      this.props.navigation.navigate("WelcomeScreen");
+      props.navigation.navigate("WelcomeScreen");
     } catch (err) {
       alert("Unable to sign out right now. Please try again later.");
     }
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <CustomActionButton
-          style={{
-            width: 200,
-            backgroundColor: "transparent",
-            borderWidth: 0.5,
-            borderColor: "#9C9C9C"
-          }}
-          title="Logout"
-          onPress={this.signOut}
-        >
-          <Text style={{ fontWeight: "500", color: "#9C9C9C" }}>Logout</Text>
-        </CustomActionButton>
-      </View>
-    );
-  }
-}
+  return (
+    <View style={styles.container}>
+      <CustomActionButton
+        style={{
+          width: 200,
+          backgroundColor: "transparent",
+          borderWidth: 0.5,
+          borderColor: "#9C9C9C"
+        }}
+        title="Logout"
+        onPress={signOut}
+      >
+        <Text style={{ fontWeight: "500", color: "#9C9C9C" }}>Logout</Text>
+      </CustomActionButton>
+    </View>
+  );
+};
 export default SettingScreen;
 
 const styles = StyleSheet.create({
